84f5ec515cbaca1c9e29a3946fdadffa2f3026ac
bayuuuaji <bayuuuajisantoso@gmail.com>
2022-01-08 19:15:55 +0700

    membuat hero

---
+++ src/components/Hero.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="row mt-4">
+    <div class="col align-self-center">
+      <h2>
+        <strong>Pesan Makanan Enak</strong> <br />
+        dari HPmu
+      </h2>
+      <p>Ayo segera pesan makanan favorit anda!</p>
+      <button class="btn btn-lg btn-success"> Pesan<b-icon-arrow-down-circle class="ml-2"></b-icon-arrow-down-circle> </button>
+    </div>
+    <div class="col">
+      <img src="../assets/images/ilustrasi.png" width="100%" alt="" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hero',
+};
+</script>
+
+<style></style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav class="navbar navbar-expand-md navbar-light">
+    <div class="container">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Kulineran</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <router-link class="nav-link" to="/">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/foods">Food</router-link>
+            </li>
+          </ul>
+          <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <router-link class="nav-link" to="/keranjang">Keranjang
+              <b-icon-cart4></b-icon-cart4>
+              <span class="badge badge-success ml-2">0</span>
+              </router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+};
+</script>
+
+<style></style>
